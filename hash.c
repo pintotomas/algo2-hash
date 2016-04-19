1752df0c82e2043f4b8f3e3b8cfdbef6aeb50fdf
t0mixcs <tomasp834@gmail.com>
2016-04-19 12:52:58 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -17,6 +17,35 @@ char *strdup(const char *old) {
     return new;
 }
 
+//Devuelve true si el numero recibido por parametro es n, si no, false
+bool es_primo(size_t n);
+bool es_primo(size_t n){
+	size_t i;
+	for (i = 2; i < n; i++){ //ya se que es divisible por si mismo y por 1
+		if (n % i == 0){
+			return false;
+			}
+		}
+	return true;
+	}
+
+//busca un numero primo entre los valores principio y fin
+//pre: principio debe ser menor que fin
+//post: devuelve el mayor numero primo entre mayor y fin 
+size_t encontrar_proximo_primo(size_t principio, size_t fin){
+
+size_t encontrar_proximo_primo(size_t principio, size_t fin);
+	size_t primo_encontrado;
+	size_t j;
+	for(j = principio+1; j <= fin; j++){ //asegurarse de que siempre hay un primo entre principio y fin
+		if (es_primo(j)){
+			primo_encontrado = j;
+			}
+		}
+	return primo_encontrado;
+	}
+			
+	
 unsigned long djb2(const char *str);
 
 /* djb2
@@ -48,22 +77,10 @@ unsigned long hash_generar_clave(const char clave[],size_t tam_hash){
 typedef struct nodo { 
     const char *clave;
 	void *valor;
-	bool ocupado;
+	bool ocupado; //me parece que esta variable se puede borrar despues (usar el mismo clave como ocupado)
 	bool borrado;
 	} nodo_hash_t;
 
-/*nodo_hash_t nuevo_nodo(void);
-
-nodo_hash_t nuevo_nodo(void){
-    
-    nodo_hash_t nodo;
-    nodo.clave = NULL;
-    nodo.valor = NULL;
-    nodo.ocupado = false;
-    nodo.borrado = false;
-    return nodo;
-    }
-*/
 typedef struct hash {
 	nodo_hash_t *tabla;
 	size_t capacidad;
@@ -109,66 +126,94 @@ size_t hash_cantidad(const hash_t *hash){
     }
 
 void hash_destruir(hash_t *hash){
+	//iterar para que borre las claves o libere valores
     free(hash->tabla);
     free(hash);
     }
 
 bool hash_guardar(hash_t *hash, const char *clave, void *dato){
+	bool insertado = false;
     unsigned long posicion = hash_generar_clave(clave,hash->capacidad);
     printf("La posicion sera.. %lu\n",posicion);
     char *clave_copia = strdup(clave);
+	if (!clave_copia){return false;}
     printf("la copia es: %s su direccion de memoria es %p y la vieja era %p\n",clave_copia,(void*)&clave_copia,(void*)&clave);
     //aca primero ver el factor de carga para redimensionar
-    bool insertado = false;
+    
     
     while (!insertado){
-        puts("Se va a hacer la primer comparacion");
-        if ((hash->tabla[posicion].clave) && (strcmp(hash->tabla[posicion].clave,clave_copia)) == 0){
-            free(clave_copia);
-            break;
-            }
-        puts("Se va a hacer la segunda comparacion");
-        if((hash->tabla[posicion].ocupado == false) || (hash->tabla[posicion].borrado == true)){
-            puts("Se entro al else if");
-            hash->tabla[posicion].ocupado = true;
-            hash->tabla[posicion].borrado = false;
-            hash->tabla[posicion].valor = dato;
-            hash->tabla[posicion].clave = clave_copia;
-            insertado = true;
-            }
-        else {
-            posicion+=1;
-            }
-        }
+		if (!hash->tabla[posicion].clave){ //Esto significa que la clave vale NULL, => esta libre
+			hash->tabla[posicion].ocupado = true;
+        	hash->tabla[posicion].borrado = false;
+        	hash->tabla[posicion].valor = dato;
+        	hash->tabla[posicion].clave = clave_copia;
+			hash->cantidad_elementos++;
+        	insertado = true;
+			}
+		else{ //La clave != NULL
+			if((strcmp(hash->tabla[posicion].clave,clave_copia)) == 0){ //lo que se habia guardado fue borrado O las claves son las mismas
+				//if destruir dato ... 
+				free((void*)hash->tabla[posicion].clave); //copias que hace alan, las elimina alan
+				hash->tabla[posicion].clave = clave_copia;
+				hash->tabla[posicion].ocupado = true;
+        		hash->tabla[posicion].borrado = false;
+        		hash->tabla[posicion].valor = dato;
+        		insertado = true;
+				hash->cantidad_elementos++;
+				}
+			else{ //no fue borrada, la clave esta en uso y es distinta => esta ocupado
+				posicion++;
+				if (posicion == hash->capacidad){
+					posicion = 0;
+					}
+				}
+			}
+		}
     return insertado;
-    }   
-    
+    }
 
 void *hash_borrar(hash_t *hash, const char *clave){
     return NULL;
+	
     }
 void *hash_obtener(const hash_t *hash, const char *clave){
-    return NULL;
+	unsigned long posicion = hash_generar_clave(clave,hash->capacidad);
+    void* dato = NULL;
+	while(hash->tabla[posicion].clave){
+		printf("Posicion vale %lu\n",posicion);
+		if((strcmp(hash->tabla[posicion].clave,clave)) == 0){
+			dato = hash->tabla[posicion].valor;
+			break;
+			}
+		else{
+			posicion++;
+			if(posicion == hash->capacidad){
+				posicion = 0;
+				}
+			
+			printf("Posicion valdra en la prox iteracion %lu\n",posicion);
+			}
+		}
+	return dato;
     }
-bool hash_pertenece(const hash_t *hash, const char *clave){
-    return false;
-    }
-int main(){
-    hash_t* new_table = hash_crear(NULL);
-    printf("La capacidad de nt es %d\n",new_table->capacidad);
-    for (size_t k = 0; k < TAMANO_INICIAL; k++){
-        printf("Deberese ser null: %p\n",new_table->tabla[k].valor);
-        }
-	char clave1[] = "gato";
-    int a = 3;
-    int *b = &a;
-	bool blabla = hash_guardar(new_table,clave1,(void*)b);
 
-    if (blabla){puts("KE");}
-    for (size_t k = 0; k < TAMANO_INICIAL; k++){
-        printf("Deberese ser null: %p\n",new_table->tabla[k].valor);
-        }
-    free((void*)new_table->tabla[5].clave);  //cuando vaya a borrar un elemento voy a tener que hacer un casteo
-    hash_destruir(new_table);
-	return 0;
-	}
+bool hash_pertenece(const hash_t *hash, const char *clave){
+    unsigned long posicion = hash_generar_clave(clave,hash->capacidad);
+	bool pertenece = false;
+	while(hash->tabla[posicion].clave){
+		printf("Posicion vale %lu\n",posicion);
+		if((strcmp(hash->tabla[posicion].clave,clave)) == 0){
+			pertenece = true;
+			break;
+			}
+		else{
+			posicion++;
+			if(posicion == hash->capacidad){
+				posicion = 0;
+				}
+			
+			printf("Posicion valdra en la prox iteracion %lu\n",posicion);
+			}
+		}
+	return pertenece;
+    }
